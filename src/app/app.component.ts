bfcccefdcd74a64f6e80ed59c6193d1622d38937
Enkhtushig Namkhai <enkhtushig@gmail.com>
2023-01-25 15:40:39 -0800

    [website update] images added to build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,12 @@ import { trigger, transition, style, animate, query, keyframes } from '@angular/
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('routeAnim', [
-      transition('* => *', [
-        query(':enter', [
-          style({
-            backgroundColor: 'black',
-            display: 'block',
-          }),
-          animate(1000),
-        ], {optional: true}),
-      ]),
-    ])
-  ],
 })
 export class AppComponent {
   title = 'Enki';
 
-  prepareRoute(outlet: RouterOutlet) {
-    if (outlet.isActivated) return outlet.activatedRoute.snapshot.url;
-    return
-  }
+  // prepareRoute(outlet: RouterOutlet) {
+  //   if (outlet.isActivated) return outlet.activatedRoute.snapshot.url;
+  //   return
+  // }
 }
