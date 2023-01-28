2f327fdd2a8d28e408cc1fc510f1fb2748af0830
Enkhtushig Namkhai <enkhtushig@gmail.com>
2023-01-28 13:45:35 -0800

    [website update] Art page clean up
    Clean up art page code + better experience for users regarding video playback
    On hover, the video will stay playing now.
    TODO: on phone, maybe edit layout.

--- src/app/artwork/artwork.component.ts
+++ src/app/artwork/artwork.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+interface Painting {
+  id: string;
+  src:string;
+  videoSrc: string;
+  title: string;
+  description: string;
+}
 
 
 @Component({
@@ -6,34 +14,64 @@ import { Component } from '@angular/core';
   templateUrl: './artwork.component.html',
   styleUrls: ['./artwork.component.scss']
 })
-export class ArtworkComponent {
-  slides: any[];
+export class ArtworkComponent implements AfterViewInit {
+  paintings: Painting[];
+  displayedVideoId = "flight";
+
+  ngAfterViewInit() {
+    document.querySelector('#'+ this.displayedVideoId)!.classList.add('displayVideo');
+  }
+
+  onMouseEnter(painting: Painting) {
+    if (painting.id !== this.displayedVideoId) {
+      document.querySelector('#'+ this.displayedVideoId)!.classList.toggle('displayVideo');
+
+      const videoElemToDisplay : Element = document.querySelector('#'+ painting.id)!;
+      videoElemToDisplay.classList.toggle('displayVideo');
+      // videoElemToDisplay.play();
+      this.displayedVideoId = painting.id;
+    }
+  }
 
   constructor() { 
-    this.slides = [{
-      id: 0,
+    this.paintings = [{
+      id: 'flight',
       src: '../../assets/flight.png',
-      title: 'Flight',
-      subtitle: 'Soaring Through the Sky'
+      videoSrc: '../../assets/flight.mp4',
+      title: 'The Flight: ',
+      description: `Oil painting on a 40\"x30\" canvas panel. 
+      This painting came about from a feeling I felt inside. During a difficult time in my life,
+      I could always observe animals and be in awe of how strong and magestic they are. They do not complain,
+      they survive, they fly and they soar. Sometimes they may fall, but they always strive, and I've always
+      found that to be fascinating when I was a little girl. My love for eagles and what they represent,
+      drove me to create this painting. I didn't have a clear image before I started, but I knew what I
+      wanted it to feel like when someone looked at it. I wanted its wings to span a wider range, I wanted 
+      it to look just as magestic as the real one.`
     },
     {
-      id: 1,
+      id: 'float',
       src: '../../assets/float.png',
-      title: 'Floating Through Time and Space',
-      subtitle: 'Inspired by the way jellyfish magically move around, like creatures from another world.'
+      videoSrc: '../../assets/float.mp4',
+      title: 'Floating Through Time and Space: ',
+      description: ` Oil painting on a 30"x30" canvas. Inspired by the magical movement of 
+      jellyfish making their way through the waters. These creatures are never still, they are always in constant motion, which made me 
+      think about how time is always moving as well, and how the spaces around us are affected by the inhabitants. Sometimes, it's ok to just 
+      calmly float through life, we don't always have to go against the current. These creatures truly mezmerized me, and I wanted to capture this feeling. </p>
+`,
     },
-  
     {
-      id: 2,
+      id: 'hope',
       src: '../../assets/hope.png',
-      title: 'The Field of Hope',
-      subtitle: ' (ref:)'
+      videoSrc: '../../assets/hope.mp4',
+      title: 'The Field of Hope: ',
+      description: `Oil painting on a 18"x24" canvas panel.`
     },
     {
-      id: 3,
+      id: 'grace',
       src: '../../assets/grace.png',
+      videoSrc: '../../assets/hope.mp4',
       title: 'Into Grace',
-      subtitle: 'Loved painting her, (ref: )'
+      description: `Oil painting on a 18"x24" canvas panel.`
     },
   ];
   }
