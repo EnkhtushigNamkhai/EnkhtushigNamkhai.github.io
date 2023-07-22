a89bc78fd6decbf1cfad11e0937f291316bc366a
Enkhtushig Namkhai <enkhtushig@Enkhtushigs-MBP.attlocal.net>
2023-07-21 17:33:33 -0700

    Fixed styling on home  page for phone views

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,15 +9,34 @@ import SplitType from 'split-type';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
   ngOnInit() {
+    console.log('init');
     const myText = new SplitType("#my-text");
      gsap.registerPlugin(ScrollTrigger);
      ScrollTrigger.config({ ignoreMobileResize: true });
+     this.animateVideo();
      this.animateName();
      this.animateBackground();
-   
+
+    var timeout: any;
+    window.addEventListener("orientationchange", (event) => {
+      if(timeout) timeout.kill();
+      
+      timeout = gsap.delayedCall(0, () => {
+        this.animateName();
+        this.animateBackground();
+      });
+    });
   }
 
+  animateVideo() {
+     gsap.from('.asset-container', {
+      opacity: 0,
+      duration: 2,
+    });
+  }
+  
   animateName() {
     gsap.to('.char', {
       y: (window.innerHeight * .70) + 500,
@@ -29,15 +48,11 @@ export class HomeComponent implements OnInit {
   }
 
   animateBackground() {
-    gsap.from('.asset-container', {
-      opacity: 0,
-      duration: 2,
-    });
-
     var tl = gsap.timeline({ repeat: 2, repeatDelay: 1 });
     tl.from("#rest", { x: -200, duration: 1, opacity: 0, });
     tl.from("#flowers", { opacity: 0, scale: 2, duration: 1 });
     tl.from("#scribbles", { opacity: 0, duration: 1 });
   }
-}
 
+  
+}
